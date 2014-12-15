8fa08c759893ffdbdfcf71e8b8be91d9372e7a4d
Thanasis Polychronakis <thanpolas@gmail.com>
2014-12-15 18:28:55 +0200

    remove css and js linting from grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -372,8 +372,6 @@ module.exports = function (grunt) {
     'clean:server',
     'jekyll:check',
     'sass:server',
-    'jshint:all',
-    'csslint:check'
   ]);
 
   grunt.registerTask('build', [
