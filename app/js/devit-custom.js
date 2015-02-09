326062883d544151117b6c72f6c7cafd74a9f87a
Thanasis Polychronakis <thanpolas@gmail.com>
2015-02-10 01:43:22 +0200

    Add signup form js

--- app/js/devit-custom.js
+++ app/js/devit-custom.js
@@ -48,4 +48,80 @@
     devit.init();
   });
 
+
+
+  //
+  //
+  // Signup Form
+  //
+  //
+  var newsletter = {};
+  // mailchimp code
+  newsletter.mceInit = function() {
+    var options = {
+      url: 'http://check-connectivity.us2.list-manage.com/subscribe/post-json?u=249dbe460c3c1857a489dde05&amp;id=faa2000c02&c=?',
+      // url: 'http://netscan.us2.list-manage2.com/subscribe/post',
+      type: 'GET',
+      dataType: 'json',
+      contentType: 'application/json; charset=utf-8'
+
+    };
+
+    $('.mc-embed-signup form').submit(function(ev){
+      $('.response').hide();
+      ev.preventDefault();
+
+      // form position
+      var email = this.EMAIL.value;
+      options.data = $(this).serialize();
+      options.success = function(resp) {
+        newsletter.mceSuccess(resp, email);
+      };
+
+      $.ajax(options);
+
+      return false;
+    });
+  };
+
+  newsletter.mceSuccess = function(resp) {
+    if (resp.result === 'success'){
+      // Show thank
+      $('.mc-embed-signup').hide();
+      $('.thankyou').removeClass('hide');
+      ga('send', 'pageview', {
+        'page': '/signup-frontpage',
+        'title': 'User Signup'
+      });
+      return;
+    }
+
+    var index = -1;
+    var msg;
+    try {
+      var parts = resp.msg.split(' - ',2);
+      if (parts[1] === undefined){
+        msg = resp.msg;
+      } else {
+        var i = parseInt(parts[0], 10);
+        if (i.toString() === parts[0]){
+          index = parts[0];
+          msg = parts[1];
+        } else {
+          index = -1;
+          msg = resp.msg;
+        }
+      }
+    } catch(e) {
+      index = -1;
+      msg = resp.msg;
+    }
+    $('.js-error').show();
+    $('.js-error').html(msg);
+  };
+
+  newsletter.mceInit();
+
+
+
 })(jQuery);
