aaaf4839bf4f954ac22f3571ff4378ac0f964df6
Vaios Karampinis <oden@oden.gr>
2017-05-07 13:38:56 +0300

    scaffold devit-week

---
+++ _js/devit-week.js
@@ -0,0 +1,23 @@
+require('./utils/common.js');
+
+require.ensure(['gmap3/dist/gmap3.min', './utils/devit-custom.js'], function (require) {
+
+  require('./utils/devit-custom.js');
+
+  var venueAddress = [40.6252233, 22.9495758];
+
+  require('gmap3/dist/gmap3.min');
+  $('#map-day-1').gmap3({
+    center: venueAddress,
+    zoom: 16,
+    streetViewControl: true,
+    panControl: false,
+    zoomControl: true,
+    scrollwheel: false,
+    mapTypeControl: false,
+  })
+  .marker({
+    position: venueAddress,
+  });
+});
+
