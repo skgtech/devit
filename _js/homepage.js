c480a13edc34178f76c5b0073a7cfee45df4d7b4
Kostas Bariotis <konmpar@gmail.com>
2017-05-18 15:32:58 +0300

    add workshops venue to home page

--- _js/homepage.js
+++ _js/homepage.js
@@ -5,6 +5,7 @@ require.ensure(['gmap3/dist/gmap3.min', './utils/devit-custom.js'], function (re
   require('./utils/devit-custom.js');
 
   var venueAddress = [40.6252233, 22.9495758];
+  var workshopAddress = [40.6373756, 22.9370313];
 
   require('gmap3/dist/gmap3.min');
   $('#map-canvas').gmap3({
@@ -18,6 +19,18 @@ require.ensure(['gmap3/dist/gmap3.min', './utils/devit-custom.js'], function (re
   })
   .marker({
     position: venueAddress,
+  });
+  $('#map-workshop-canvas').gmap3({
+    center: workshopAddress,
+    zoom: 16,
+    streetViewControl: true,
+    panControl: false,
+    zoomControl: true,
+    scrollwheel: false,
+    mapTypeControl: false,
+  })
+  .marker({
+    position: workshopAddress,
   });
 });
 
