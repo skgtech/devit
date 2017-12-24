79bde0138717e6829869c2e53ac485f375d1ce16
Kostas Bariotis <konmpar@gmail.com>
2017-12-24 23:49:00 +0200

    Update 2017-12-24T21:48:52.478Z

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,51 @@ const path = require('path');
 const fs = require('fs');
 const webpack = require('webpack');
 
-const files = fs.readdirSync('./_js/').filter(a => a.indexOf('.js') > -1).map(a => a.replace('.js', ''));
+const files = fs.readdirSync('./_js/').filter(a => a.indexOf('.js') > -1);
 const entry = {};
 
 files.forEach(a => {
-  entry[a] = a;
+  entry[a] = `${path.join(path.resolve(__dirname, '_js'), a)}`;
 });
 
 module.exports = {
   context: __dirname + '/assets',
   entry: entry,
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?(\?\-[0-9|a-z]{6})?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=assets/js/[hash].[ext]',
+        use: [
+          { loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=assets/js/[hash].[ext]',}
+        ]
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?(\?\-[0-9|a-z]{6})?$/,
-        loader: 'file-loader?name=assets/js/[hash].[ext]',
+        use: [
+          { loader: 'file-loader?name=assets/js/[hash].[ext]',}
+        ]
       },
       {
         test: /\.(sass|scss)?$/,
-        loader: 'style!css?minimize!sass',
+        use: [
+          { loader: 'style-loader'},
+          { loader: 'css-loader?minimize'},
+          { loader: 'sass-loader'},
+        ]
       },
       {
         test: /\.(css)?$/,
-        loader: 'style!css?minimize',
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader?minimize' },
+        ]
       },
     ],
   },
   resolve: {
-    modulesDirectories: [
-      path.resolve(__dirname, 'node_modules'),
-      path.resolve(__dirname, '_js'),
-    ],
-    root: [
-      path.resolve(__dirname, 'node_modules'),
-      path.resolve(__dirname, '_js'),
-    ],
+    alias: {
+      _js: path.resolve(__dirname, '_js'),
+    },
   },
   output: {
     path: path.join(__dirname, '_js'),
