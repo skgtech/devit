46ecf7bfb3590b64708e3c47ea38fc1e2c5a6411
Kostas Bariotis <konmpar@gmail.com>
2017-12-27 23:11:06 +0200

    sticky menu and typography fixes

--- _js/modules/layout.js
+++ _js/modules/layout.js
@@ -3,6 +3,16 @@ require('bootstrap-sass/assets/javascripts/bootstrap/transition');
 require('bootstrap-sass/assets/javascripts/bootstrap/collapse');
 require('lightslider/dist/js/lightslider.min');
 
-const stickyMenu = require('./sticky-menu');
+// const stickyMenu = require('./sticky-menu');
 
-stickyMenu('sticky');
+// stickyMenu('sticky');
+
+const Headhesive = require('headhesive');
+
+const header = new Headhesive('.sticky', {
+  classes: {
+    clone: 'sticky-menu--clone',
+    stick: 'sticky-menu--stick'
+  }
+});
+
