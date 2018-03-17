531ec86ec5ac35ca5317a11bf6858f54b062a9aa
Kostas Bariotis <konmpar@gmail.com>
2018-03-17 14:30:10 +0200

    finish single speaker page

--- _js/modules/layout.js
+++ _js/modules/layout.js
@@ -1,6 +1,7 @@
 window.jQuery = window.$ = require('jquery');
 require('bootstrap-sass/assets/javascripts/bootstrap/transition');
 require('bootstrap-sass/assets/javascripts/bootstrap/collapse');
+require('bootstrap-sass/assets/javascripts/bootstrap/tab');
 require('lightslider/dist/js/lightslider.min');
 
 require('./sticky-menu')('sticky');
