3ea9037c8aee9db0935c2f4652e943086858f5cf
Kostas Bariotis <konmpar@gmail.com>
2018-03-17 17:33:18 +0200

    initial release with responsive images, kind of

--- Gemfile
+++ Gemfile
@@ -3,7 +3,4 @@ ruby "2.4.2"
 
 gem "jekyll", "3.3.0"
 gem "html-proofer"
-
-group :jekyll_plugins do
-   gem "jekyll-feed", "~> 0.6"
-end
+gem "jekyll-responsive-image"
--- _js/modules/layout.js
+++ _js/modules/layout.js
@@ -4,6 +4,12 @@ require('bootstrap-sass/assets/javascripts/bootstrap/collapse');
 require('bootstrap-sass/assets/javascripts/bootstrap/tab');
 require('lightslider/dist/js/lightslider.min');
 
+var LazyLoad = require('vanilla-lazyload/dist/lazyload.min');
+
+new LazyLoad({
+  elements_selector: ".lazy"
+});
+
 require('./sticky-menu')('sticky');
 require('./slack');
 require('./venues');
