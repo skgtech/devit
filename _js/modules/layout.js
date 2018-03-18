31cb52675c2cb23ea1505f70e4237a22ee545d1c
Kostas Bariotis <konmpar@gmail.com>
2018-03-18 13:08:46 +0200

    refinements

--- _js/modules/layout.js
+++ _js/modules/layout.js
@@ -4,6 +4,15 @@ require('bootstrap-sass/assets/javascripts/bootstrap/collapse');
 require('bootstrap-sass/assets/javascripts/bootstrap/tab');
 require('lightslider/dist/js/lightslider.min');
 
+var tabs = document.querySelectorAll('.talks__tabs ul li:first-child');
+var tabsContent = document.querySelectorAll('.talks__content .tab-pane:first-child');
+if (tabs.length) {
+  tabs[0].classList.add('active');
+}
+if (tabsContent.length) {
+  tabsContent[0].classList.add('active');
+}
+
 var LazyLoad = require('vanilla-lazyload/dist/lazyload.min');
 
 new LazyLoad({
