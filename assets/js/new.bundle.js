a18b6c7fa873ab60151302ad2b3fcfcf70be10b1
Kostas Bariotis <konmpar@gmail.com>
2018-03-18 21:01:59 +0200

    Update 2018-03-18T19:01:54.894Z

--- assets/js/new.bundle.js
+++ assets/js/new.bundle.js
@@ -1,1 +1,1 @@
-webpackJsonp([15],{36:function(t,o,e){$(document).ready(function(){$("#autoWidth").lightSlider({controls:!0,pager:!0,item:3,adaptiveHeight:!0,loop:!0,onSliderLoad:function(){$.getScript("https://platform.twitter.com/widgets.js",function(){console.log("twitter loaded")})}})}),e.e(2).then(e.bind(null,43))}},[36]);
+webpackJsonp([15],{37:function(n,e,c){$(document).ready(function(){}),c.e(2).then(c.bind(null,44))}},[37]);
