8a13cfea694f2e851a284d862b9a8f9e74ff0dc7
Kostas Bariotis <konmpar@gmail.com>
2018-04-05 19:52:47 +0300

    fixes

--- _plugins/speakers_generator.rb
+++ _plugins/speakers_generator.rb
@@ -26,7 +26,7 @@ module Jekyll
       self.data['year'] = year
       self.data['title'] = "#{ speaker['first_name'] } #{speaker['last_name']}"
       self.data['description'] = "#{ speaker['bio'].gsub(/<\/?[^>]*>/, "")[0, 150] } ..."
-      self.data['image'] = "/assets/images/speakers/#{year}/#{ speaker['url'] }.png"
+      self.data['image'] = "/assets/images/speakers/#{year}/#{ speaker['image_filename'] ? speaker['image_filename'] : speaker['url'] }.png"
     end
   end
 
