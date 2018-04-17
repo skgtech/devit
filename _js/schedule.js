1d80b51f0c01d7e5da3ce283a484a7fd92d2a9d8
Kostas Bariotis <konmpar@gmail.com>
2018-04-18 00:03:28 +0300

    initial schedule

---
+++ _js/schedule.js
@@ -0,0 +1,33 @@
+require('bootstrap-sass/assets/javascripts/bootstrap/tab');
+
+var tabs = document.querySelectorAll('.schedule__tabs ul li:first-child');
+var tabsContent = document.querySelectorAll('.schedule__content .tab-pane:first-child');
+
+if (tabs.length) {
+  tabs[0].classList.add('active');
+}
+if (tabsContent.length) {
+  tabsContent[0].classList.add('active');
+}
+
+////////////////////////
+
+var tabs = document.querySelectorAll('.workshops__tabs ul li:first-child');
+var tabsContent = document.querySelectorAll('.workshops__content .tab-pane:first-child');
+
+if (tabs.length) {
+  tabs[0].classList.add('active');
+}
+if (tabsContent.length) {
+  tabsContent[0].classList.add('active');
+}
+
+var hash = window.location.hash;
+hash && $('ul.schedule__tabs a[href="' + hash + '"]').tab('show');
+
+$('.schedule__tabs a').click(function (e) {
+  $(this).tab('show');
+  var scrollmem = $('body').scrollTop() || $('html').scrollTop();
+  window.location.hash = this.hash;
+  $('html,body').scrollTop(scrollmem);
+});
