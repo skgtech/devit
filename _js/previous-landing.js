8ab6617ae0182fff8344feed1de81f289b227da0
kbariotis <konmpar@gmail.com>
2019-06-18 20:49:20 +0100

    only auto stop

--- _js/previous-landing.js
+++ _js/previous-landing.js
@@ -1,8 +1,6 @@
 require('bootstrap-sass/assets/javascripts/bootstrap/modal');
 
-$('#video-modal').on('shown.bs.modal', function () {
-  $('.moments-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
-})
+// auto stop video when modal closes
 $('#video-modal').on('hidden.bs.modal', function () {
   $('.moments-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
 })
