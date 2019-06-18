ebf0cef91b60d022e70ed868eb4a3bc62d6e1c78
kbariotis <konmpar@gmail.com>
2019-06-18 20:43:01 +0100

    automatically play/stop video

--- _js/previous-landing.js
+++ _js/previous-landing.js
@@ -1,1 +1,8 @@
 require('bootstrap-sass/assets/javascripts/bootstrap/modal');
+
+$('#video-modal').on('shown.bs.modal', function () {
+  $('.moments-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
+})
+$('#video-modal').on('hidden.bs.modal', function () {
+  $('.moments-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+})
